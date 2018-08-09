f34b60d455067b6e8d179b04cde163c9fa6f38d9
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2018-08-10 02:16:38 +0600

    now can save user and redirect to homepage

--- apps/crud_app/views.py
+++ apps/crud_app/views.py
@@ -1,13 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from .models import User
 
 # Create your views here.
 def get_index_page(request):
     return render(request,'index.html');
 def get_about_page(request):
     return render(request,'about.html');
+
+@csrf_protect
 def register_user(request):
     if(request.method == 'GET'):
         return render(request, 'register.html');
-    else:
-        return HttpResponse('It is a post request');
+    else:#request is post
+        #getting post parameters
+        name = request.POST.get("name");
+        email = request.POST.get("email");
+        phoneno = request.POST.get("phoneno");
+        password = request.POST.get("pwd");
+        user = User()
+        user.name = name
+        user.email = email
+        user.password = password
+        user.phone_number = phoneno
+        user.save();
+        request.session['name'] = name
+        request.session['email'] = name
+        request.session['phoneno'] = phoneno
+        return redirect('/app');
--- postgres_project/settings.py
+++ postgres_project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'music_db',
         'USER': 'postgres',
-        'PASSWORD': 'binate',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
